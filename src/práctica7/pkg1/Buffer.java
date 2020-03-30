9d22c6824c31116a05bf2b0ef60f8de67b65829f
Sergio <storres@cifpfbmoll.eu>
2020-03-30 12:44:45 +0200

    Primera expo

--- src/práctica7/pkg1/Buffer.java
+++ src/práctica7/pkg1/Buffer.java
@@ -21,23 +21,24 @@ public class Buffer {
                 BufferedWriter salida = new BufferedWriter(new FileWriter(destino))) {
             String linealeida = lector.readLine();
             String[] peliculas = linealeida.split("\\{");
-            String[][] apartado_peliculas = null;
+            String[][] apartado_peliculas = new String [peliculas.length][elementos.length+1];
             for (int j = 0; j < peliculas.length; j++) {
                 apartado_peliculas[j] = peliculas[j].split("#");
             }
-            salida.newLine();
 
-            for (int j = 0; j < apartado_peliculas[j].length; j++) {
-                for (int k=0; k < apartado_peliculas[j][k].length(); k++){
+            for (int j = 0; j < apartado_peliculas.length; j++) {
+                for (int k=0; k < apartado_peliculas[0].length; k++){
                     salida.write(apartado_peliculas[j][k]);
-                }
-                salida.newLine();
-                if (i < 6) {
+                    salida.newLine();
+                    if (i < 6) {
                     salida.write(elementos[i]);
                     i++;
                 } else {
                     i = 0;
                 }
+                }
+                salida.newLine();
+                
             }
             salida.close();
         } catch (IOException exc) {
